c1846efdcfdfb9a46d879206ccdcb0734966a898
sbx_user1051 <sbx_user1051@169.254.62.1>
2026-01-06 19:33:19 +0000

    Tue Jan 06 19:33:19 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -17,10 +17,33 @@ def test_rule_of_3() -> None:
     run_queue(
         [
             call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
-            call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
-            call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
-            call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
+            call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(2),
+            call_enqueue("id_verification", 1, iso_ts(delta_minutes=0)).expect(3),
+            call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(4),
             call_dequeue().expect("companies_house", 1),
+            call_dequeue().expect("id_verification", 1),
+            call_dequeue().expect("bank_statements", 1),
+            call_dequeue().expect("bank_statements", 2),
         ]
     )
+
+
+def test_rule_of_3_with_timestamps() -> None:
+    run_queue(
+        [
+            call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
+            call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(2),
+            call_enqueue("id_verification", 1, iso_ts(delta_minutes=0)).expect(3),
+            call_enqueue("companies_house", 2, iso_ts(delta_minutes=5)).expect(4),
+            call_enqueue("id_verification", 2, iso_ts(delta_minutes=6)).expect(5),
+            call_enqueue("bank_statements", 2, iso_ts(delta_minutes=7)).expect(6),
+            call_dequeue().expect("companies_house", 2),
+            call_dequeue().expect("id_verification", 2),
+            call_dequeue().expect("bank_statements", 2),
+            call_dequeue().expect("companies_house", 1),
+            call_dequeue().expect("id_verification", 1),
+            call_dequeue().expect("bank_statements", 1),
+        ]
+    )
+
 
