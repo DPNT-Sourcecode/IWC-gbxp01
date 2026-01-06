ca5e08be834390b9c254d794c1febb6cb3af1f54
sbx_user1051 <sbx_user1051@169.254.62.1>
2026-01-06 21:38:01 +0000

    Tue Jan 06 21:38:01 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -32,11 +32,11 @@ def test_rule_of_3_with_timestamps() -> None:
     run_queue(
         [
             call_enqueue("companies_house", 1, iso_ts(delta_minutes=5)).expect(1),
-            call_enqueue("bank_statements", 1, iso_ts(delta_minutes=6)).expect(2),
+            call_enqueue("bank_statements", 1, iso_ts(delta_minutes=3)).expect(2),
             call_enqueue("id_verification", 1, iso_ts(delta_minutes=7)).expect(3),
             call_enqueue("companies_house", 2, iso_ts(delta_minutes=0)).expect(4),
             call_enqueue("id_verification", 2, iso_ts(delta_minutes=1)).expect(5),
-            call_enqueue("bank_statements", 2, iso_ts(delta_minutes=2)).expect(6),
+            call_enqueue("bank_statements", 2, iso_ts(delta_minutes=6)).expect(6),
             call_dequeue().expect("companies_house", 2),
             call_dequeue().expect("id_verification", 2),
             call_dequeue().expect("bank_statements", 2),
@@ -251,6 +251,7 @@ def test_same_timestamp_bank_statements_and_companies_house() -> None:
             call_dequeue().expect("id_verification", 6),
         ]
     )
+
 
 
 
