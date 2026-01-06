b184ea9f7f0b448395784f277a8cbfd513b5f584
sbx_user1051 <sbx_user1051@169.254.62.1>
2026-01-06 21:29:01 +0000

    Tue Jan 06 21:29:01 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -240,3 +240,17 @@ def test_time_sensitive_bank_statements_exactly_5_minutes() -> None:
     )
 
 
+def test_same_timestamp_bank_statements_and_companies_house() -> None:
+    run_queue(
+        [
+            call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
+            call_enqueue("bank_statements", 2, iso_ts(delta_minutes=5)).expect(2),
+            call_enqueue("id_verification", 2, iso_ts(delta_minutes=5)).expect(2),
+            call_dequeue().expect("bank_statements", 1),
+            call_dequeue().expect("companies_house", 2),
+            call_dequeue().expect("id_verification", 2),
+        ]
+    )
+
+
+
